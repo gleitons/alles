d44bbb1e08bdd65f01fabff381a7854bbc0219df
gleitons <gleitons@icloud.com>
2023-07-07 06:25:50 -0300

    a

---
+++ assets/js/atual.js
@@ -0,0 +1,121 @@
+
+
+const geral = () => { document.querySelector('#atualizado').innerHTML = `  <div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=555180652808&text=Ol%C3%A1,%20gostaria%20de%20adquirir%20o%20dom%C3%ADnio%20https://www.allesestofados.com.br/" target="_blank">
+    <button class="iconClick buy compra">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Comprar esse domínio</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=5551998360135" target="_blank">
+    <button class="iconClick whats">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Whatsapp</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="tel:5131332163" target="_blank">
+    <button class="iconClick fale">
+        <i class="bi bi-telephone-plus-fill"></i>
+        <h3>Fale Conosco</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=555180652808&text=Ol%C3%A1,%20gostaria%20de%20adquirir%20o%20dom%C3%ADnio%20https://www.allesestofados.com.br/" target="_blank">
+    <button class="iconClick buy compra">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Comprar esse domínio</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://g.page/r/Cfe8IJIzujB4EAg/review" target="_blank">
+    <button class="iconClick fale">
+        <i class="bi bi-star-fill"></i>
+        <h3>Nos Avalie no Google</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://maps.app.goo.gl/Xepb8GK4eLmpcH5L6" target="_blank">
+    <button class="iconClick fale">
+        <i class="bi bi-geo-alt-fill"></i>
+        <h3>Nossa Localização</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=555180652808&text=Ol%C3%A1,%20gostaria%20de%20adquirir%20o%20dom%C3%ADnio%20https://www.allesestofados.com.br/" target="_blank">
+    <button class="iconClick buy compra">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Comprar esse domínio</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://g.co/kgs/a659Qt" target="_blank">
+    <button class="iconClick fale">
+        <i class="bi bi-google"></i>
+        <span>
+            <h3>Nosso Perfil no Google</h3>
+        </span>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=555180652808&text=Ol%C3%A1,%20gostaria%20de%20adquirir%20o%20dom%C3%ADnio%20https://www.allesestofados.com.br/" target="_blank">
+    <button class="iconClick buy compra">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Comprar esse domínio</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://www.instagram.com/allesestofados/" target="_blank">
+    <button class="iconClick insta">
+        <i class="bi bi-instagram"></i>
+        <h3>Instagram</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=555180652808&text=Ol%C3%A1,%20gostaria%20de%20adquirir%20o%20dom%C3%ADnio%20https://www.allesestofados.com.br/" target="_blank">
+    <button class="iconClick buy compra">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Comprar esse domínio</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://www.facebook.com/alles.estofados" target="_blank">
+    <button class="iconClick face">
+        <i class="bi bi-facebook"></i>
+        <h3>Facebook</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://m.uber.com/ul/?client_id=&action=setPickup&pickup%5Bgeolocation%5D&pickup%5Bnickname%5D=Uber%20Meu%20Local&dropoff%5Blatitude%5D=-29.66933750&dropoff%5Blongitude%5D=-51.14366080&dropoff%5Bnickname%5D=Alles%20Estofados%20" target="_blank">
+    <button class="iconClick uber">
+        <i class="bi bi-car-front-fill"></i>
+        <h3>Uber</h3>
+    </button>
+</a>
+</div>
+<div class="buttonC ">
+<a href="https://api.whatsapp.com/send?phone=555180652808&text=Ol%C3%A1,%20gostaria%20de%20adquirir%20o%20dom%C3%ADnio%20https://www.allesestofados.com.br/" target="_blank">
+    <button class="iconClick buy compra">
+        <i class="bi bi-whatsapp"></i>
+        <h3>Comprar esse domínio</h3>
+    </button>
+</a>
+</div>
+`
+}
+
+setTimeout('geral()', 4000)
+
